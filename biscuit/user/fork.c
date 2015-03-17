5f6ba64efc754009eaa17100ce7ca5acdcfc78ee
Cody Cutler <ccutler@mat.lcs.mit.edu>
2015-03-17 13:33:42 -0400

    remove busy waits from fork

--- biscuit/user/fork.c
+++ biscuit/user/fork.c
@@ -2,43 +2,23 @@
 
 void child(int id)
 {
-	int i, j;
-	for (i = 0; i < 6; i++) {
+	int i;
+	for (i = 0; i < 3; i++) {
 		printf("hello from %d\n", id);
-		for (j = 0; j < 10000000; j++)
-			asm volatile("":::"memory");
 	}
-	int pid = fork();
-	if (!pid) {
-		for (i = 0; i < 6; i++) {
-			printf("hello from baby %d\n", id);
-			for (j = 0; j < 10000000; j++)
-				asm volatile("":::"memory");
-		}
-	}
-	exit(pid);
+	exit(id);
 }
 
 int main(int argc, char **argv)
 {
 	int pid = 0;
 	int id = 0;
-	while (pid < 10000) {
+	while (pid < 1000) {
 		pid = fork();
 		id++;
 		if (!pid)
 			child(id);
 	}
-
-	//int id = 0;
-	//int pid = 0;
-	//while (1) {
-	//	pid = fork();
-	//	if (pid)
-	//		exit(id);
-	//	id++;
-	//	printf("spawn %d\n", id);
-	//}
 	printf("parent done!\n");
 	return 0;
 }
