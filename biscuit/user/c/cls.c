05776eec725548361b9271f126eb758f671ffaf9
Jon Gjengset <jon@thesquareplanet.com>
2015-05-13 17:54:01 -0400

    Make cls overwrite all 79x24 positions

--- biscuit/user/c/cls.c
+++ biscuit/user/c/cls.c
@@ -2,11 +2,13 @@
 
 int main(int argc, char **argv)
 {
-	char buf[2000];
+	char buf[81];
 	int i;
-	for (i = 0; i < sizeof(buf); i++)
+	for (i = 0; i < 79; i++)
 		buf[i] = ' ';
+	buf[sizeof(buf) - 2] = '\n';
 	buf[sizeof(buf) - 1] = 0;
-	printf("%s", buf);
+	for (i = 0; i < 24; i++)
+		printf("%s", buf);
 	return 0;
 }
