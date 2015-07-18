24c59e8c800f22e58b0ff245d267a029e6d693f1
Cody Cutler <ccutler@mat.lcs.mit.edu>
2015-07-18 11:25:35 -0400

    rusage info to time(1)
    and -r switch to enable kernel profiling

--- biscuit/user/c/time.c
+++ biscuit/user/c/time.c
@@ -11,34 +11,63 @@ ulong now()
 	return t.tv_sec * 1000 + t.tv_usec / 1000;
 }
 
+void usage(char *me)
+{
+	errx(-1, "usage: %s [-r] <command> <arg1> ...", me);
+}
+
 int main(int argc, char **argv)
 {
-	if (argc < 2)
-		errx(-1, "usage: %s <command> <arg1> ...", argv[0]);
+	char *me = argv[0];
+	int profile = 0;
+	int ch;
+	while ((ch = getopt(argc, argv, "r")) != -1) {
+		switch (ch) {
+		case 'r':
+			profile = 1;
+			break;
+		default:
+			usage(me);
+			break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc < 1)
+		usage(me);
 
 	ulong start = now();
 
 	// start profiling
-	if (fake_sys(1))
+	if (profile && fake_sys(1))
 		errx(-1, "prof start");
 
 	if (fork() == 0) {
 		int ret;
-		ret = execvp(argv[1], &argv[1]);
+		//ret = execvp(argv[1], &argv[1]);
+		ret = execvp(argv[0], &argv[0]);
 		err(ret, "execv");
 	}
 
+	struct rusage r;
 	int status;
-	wait(&status);
+	int ret = wait4(WAIT_ANY, &status, 0, &r);
+	if (ret < 0)
+		err(ret, "wait4");
 	ulong elapsed = now() - start;
 
 	// stop profiling
-	if (fake_sys(0))
+	if (profile && fake_sys(0))
 		errx(-1, "prof stop");
 
 	if (!WIFEXITED(status) || WEXITSTATUS(status))
 		printf("child failed with status: %d\n", WEXITSTATUS(status));
 
 	printf("%lu seconds, %lu ms\n", elapsed/1000, elapsed%1000);
+	printf("user   time: %lu seconds, %lu us\n", r.ru_utime.tv_sec,
+	    r.ru_utime.tv_usec);
+	printf("system time: %lu seconds, %lu us\n", r.ru_stime.tv_sec,
+	    r.ru_stime.tv_usec);
 	return 0;
 }
