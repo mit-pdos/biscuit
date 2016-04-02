6b1a11ce73a7e51c74f014259a9e645a51a635f7
Cody Cutler <ccutler@mat.lcs.mit.edu>
2016-04-02 12:37:24 -0400

    de-stupify ls(3) and use fancy new dir stuff
    don't recursively list entire fs by default so an accidental 'ls' after running
    mailbench doesn't force me to reboot.

--- biscuit/user/c/ls.c
+++ biscuit/user/c/ls.c
@@ -1,104 +1,115 @@
 #include <litc.h>
 
- struct  __attribute__((packed)) dirent_t {
- #define NMAX	14
-	char	name[NMAX];
-	ulong	inum;
-};
-
- struct dirdata_t {
- #define NDIRENTS	(512/sizeof(struct dirent_t))
-	struct dirent_t de[NDIRENTS];
- };
-
- char *dename(char *den)
- {
-	static char nbuf[NMAX + 1];
-
-	if (den[0] == 0)
-		return NULL;
-	int i;
-	for (i = 0; i < NMAX; i++)
-		nbuf[i] = den[i];
-	nbuf[NMAX] = 0;
-	return nbuf;
- }
-
-void dprint(int fd, char *par, int left)
+void dprint(int fd, char *par, int left, int rec)
 {
-	char buf[512];
-
 	printf("%s/:\n", par);
 
 	char *pend = par + strlen(par);
 	snprintf(pend, left, "/");
 	left--;
 	pend = par + strlen(par);
-	while (read(fd, buf, sizeof(buf)) > 0) {
-		struct dirdata_t *dd = (struct dirdata_t *)buf;
-		int i;
-		for (i = 0; i < NDIRENTS; i++) {
-			char *tn = dename(dd->de[i].name);
-			if (!tn)
-				continue;
-			snprintf(pend, left, "%s", tn);
-			char *fn = par;
-			struct stat st;
-			if (stat(fn, &st))
-				err(-1, "stat");
-			char spec;
-			if (S_ISDIR(st.st_mode))
-				spec = 'd';
-			else if (S_ISSOCK(st.st_mode))
-				spec = 's';
-			else
-				spec = '-';
-			printf("%crwxr-xr-x %ld %s\n", spec, st.st_size, tn);
-		}
+
+	DIR *dir = fdopendir(fd);
+	if (!dir)
+		err(-1, "fdopendir");
+	struct dirent des, *de;
+	int ret;
+	while (1) {
+		ret = readdir_r(dir, &des, &de);
+		if (ret)
+			errx(-1, "readdir_r %s", strerror(ret));
+		if (!de)
+			break;
+		int used = snprintf(pend, left, "%s", de->d_name);
+		if (used >= left)
+			errx(-1, "long filenames!");
+		char *fn = par;
+		struct stat st;
+		if (stat(fn, &st))
+			err(-1, "stat");
+		char spec;
+		if (S_ISDIR(st.st_mode))
+			spec = 'd';
+		else if (S_ISSOCK(st.st_mode))
+			spec = 's';
+		else
+			spec = '-';
+		printf("%crwxr-xr-x %ld %s\n", spec, st.st_size, de->d_name);
+	}
+	if (!rec) {
+		if (closedir(dir) == -1)
+			err(-1, "closedir");
+		return;
 	}
 
 	// recursive list
 	*pend = 0;
-	if (lseek(fd, 0, SEEK_SET) < 0)
-		err(-1, "lseek");
-	while (read(fd, buf, sizeof(buf)) > 0) {
-		struct dirdata_t *dd = (struct dirdata_t *)buf;
-		int i;
-		for (i = 0; i < NDIRENTS; i++) {
-			char *tn = dename(dd->de[i].name);
-			if (!tn)
-				continue;
-			if (strncmp(tn, "..", 3) == 0 ||
-			    strncmp(tn, ".", 2) == 0)
-				continue;
-			snprintf(pend, left, "%s", tn);
-			char *fn = par;
-			struct stat st;
-			if (stat(fn, &st))
-				err(-1, "stat");
-			if (S_ISDIR(st.st_mode)) {
-				int tfd = open(fn, O_RDONLY | O_DIRECTORY, 0);
-				if (tfd < 0)
-					err(-1, "rec open");
-				dprint(tfd, par, left - strlen(pend));
-				if (close(tfd))
-					err(-1, "close");
-			}
+	rewinddir(dir);
+	while (1) {
+		ret = readdir_r(dir, &des, &de);
+		if (ret)
+			errx(-1, "readdir_r %s", strerror(ret));
+		if (!de)
+			break;
+		char *tn = de->d_name;
+		if (strncmp(tn, "..", 3) == 0 ||
+		    strncmp(tn, ".", 2) == 0)
+			continue;
+		snprintf(pend, left, "%s", tn);
+		char *fn = par;
+		struct stat st;
+		if (stat(fn, &st))
+			err(-1, "stat");
+		if (S_ISDIR(st.st_mode)) {
+			int tfd = open(fn, O_RDONLY | O_DIRECTORY, 0);
+			if (tfd < 0)
+				err(-1, "rec open");
+			dprint(tfd, par, left - strlen(pend), 1);
+			// dprint closes tfd
 		}
 	}
+	if (closedir(dir) == -1)
+		err(-1, "closedir");
+}
+
+__attribute__((noreturn))
+void usage(void)
+{
+	fprintf(stderr, "%s [-R]\n"
+	    "-R    recurse subdirectories\n"
+	    "\n", __progname);
+	exit(-1);
 }
 
 int main(int argc, char **argv)
 {
-	char pbuf[256] = {'.'};
-	if (sizeof(struct dirent_t) != 22)
-		errx(-1, "unexpected dirent size");
-
+	int rec = 0;
+	int ch;
+	while ((ch = getopt(argc, argv, "R")) != -1) {
+		switch (ch) {
+		case 'R':
+			rec = 1;
+			break;
+		default:
+			usage();
+			break;
+		}
+	}
+	char *start;
+	int rem = argc - optind;
+	if (rem == 0)
+		start = ".";
+	else if (rem == 1)
+		start = argv[optind];
+	else
+		usage();
 	int fd;
-	if ((fd = open("./", O_RDONLY, 0)) < 0)
-		err(-1, "open root");
+	if ((fd = open(start, O_RDONLY | O_DIRECTORY, 0)) == -1)
+		err(-1, "open %s", start);
 
-	dprint(fd, pbuf, sizeof(pbuf));
+	char pbuf[256];
+	strncpy(pbuf, start, sizeof(pbuf));
+	dprint(fd, pbuf, sizeof(pbuf) - strlen(pbuf), rec);
 	close(fd);
 
 	return 0;
