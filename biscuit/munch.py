9da18e53e0e26cc04b00df13fdf95e470a445991
Cody Cutler <ccutler@mat.lcs.mit.edu>
2016-04-18 14:05:13 -0400

    fancier PMU profile dumper

--- biscuit/munch.py
+++ biscuit/munch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import getopt
 import subprocess
 import sys
 
@@ -7,6 +8,7 @@ def usage():
 	print >> sys.stderr
 	print >> sys.stderr, 'usage: %s <redis PMU profile>' % (sys.argv[0])
 	print >> sys.stderr
+	sys.exit(-1)
 
 def openrips(fn):
 	f = open(fn)
@@ -262,50 +264,107 @@ def getsmap2(binfn):
 		ret.append((r1,r2,r3))
 	return ret
 
-def rip2func(rips, binfn):
-	smap = getsmap2(binfn)
-	funcs = {}
+def rip2func(rips, binfn, smap):
+	ipbyname = {}
 
 	si = 0
 	for _kr in rips:
 		kr = int(_kr, 16)
+		found = False
 		while True:
 			s = smap[si]
 			n = s[0]
 			low = s[1]
 			hi = s[2]
 			if kr >= low and kr < hi:
-				if n not in funcs:
-					funcs[n] = 0
-				funcs[n] += 1
+				if n not in ipbyname:
+					ipbyname[n] = []
+				ipbyname[n].append(kr)
+				found = True
 				break
 			si += 1
+		if not found:
+			raise ValueError("didn't find rip %s" % (_kr))
 	fin = []
-	for f in funcs:
-		fin.append((funcs[f], f))
+	for f in ipbyname:
+		fin.append((len(ipbyname[f]), f))
 	fin.sort()
 	fin.reverse()
-	return fin
+	return fin, ipbyname
 
-def kdump(rips):
-	samples = len(rips)
-	urips, krips = divrips(rips)
-	krips.sort()
+def disass(fname, rips, smap, binfn):
+	found = False
+	start = 0
+	end = 0
+	for s in smap:
+		if s[0] == fname:
+			found = True
+			start = s[1]
+			end = s[2]
+			break
+	if not found:
+		raise ValueError("didn't find func")
 
-	fin = rip2func(krips, kbin)
+	odcmd = ['objdump', '-d', '--start-address=%#x' % (start),
+	    '--stop-address=%#x' % (end), binfn]
+	od = subprocess.Popen(odcmd, stdout=subprocess.PIPE)
+	text, _ = od.communicate()
+	ret = []
+	for l in text.split('\n'):
+		l = l.strip()
+		if l == '':
+			continue
+		if l.find('file format') != -1:
+			continue
+		if l.find('Disassembly of') != -1:
+			continue
+		# don't try to parse ip of first line (name of function)
+		if l[0] == '0':
+			print l
+			continue
+
+		thisip = l.split()[0]
+		thisip = int(thisip[:thisip.find(':')], 16)
+		c = rips.count(thisip)
+		print '%6d %s' % (c, l)
+
+def dumpsec(secname, rips, binfn, nsamp):
+	rips.sort()
+
+	smap = getsmap2(binfn)
+	fin, ipbn = rip2func(rips, binfn, smap)
+	print '==== %s ====' % (secname)
 	tot = 0
 	for f in fin:
 		n = f[1].strip()
-		s = float(f[0])
-		tot += f[0]
-		print '%-35s %6.2f' % (n, s/samples)
+		c = f[0]
+		s = float(c)
+		tot += c
+		cs = '(%d)' % (c)
+		print '%-35s %6.2f %6s' % (n, s/nsamp, cs)
+		fname = f[1]
+		if dumpips:
+			disass(fname, ipbn[fname], smap, binfn)
 	print '---------'
-	print 'total %6.2f' % (float(tot)/samples)
+	print 'total %6.2f' % (float(tot)/nsamp)
 
-if len(sys.argv) != 2:
+def dump(rips, dumpips=False):
+	samples = len(rips)
+	urips, krips = divrips(rips)
+	dumpsec('KERNEL TIME', krips, kbin, samples)
+	dumpsec('USER   TIME', urips, ubin, samples)
+
+opts, args = getopt.getopt(sys.argv[1:], 'd')
+if len(args) != 1:
 	usage()
-prof = sys.argv[1]
+
+dumpips = False
+for o in opts:
+	if o[0] == '-d':
+		dumpips = True
+prof = args[0]
 rips = openrips(prof)
 
-manual(rips)
-kdump(rips)
+#manual(rips)
+
+dump(rips, dumpips)
