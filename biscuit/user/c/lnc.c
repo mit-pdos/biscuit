5a94d9580d74e231b584c297596882c7bd0967ef
Cody Cutler <ccutler@mat.lcs.mit.edu>
2016-11-01 19:34:50 -0400

    make lnc(1) multiplex stdin and the TCP socket

--- biscuit/user/c/lnc.c
+++ biscuit/user/c/lnc.c
@@ -15,15 +15,39 @@ int main(int argc, char **argv)
 	sin.sin_addr.s_addr = htonl(0x121a0530);
 	if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) == -1)
 		err(-1, "connect");
+	printf("connected\n");
 
-	ssize_t ret;
+	struct pollfd pfds[2] = {{.fd = 0}, {.fd = s}};
+	const int nfds = 2;
 	char buf[512];
-	while ((ret = read(s, buf, sizeof(buf) - 1)) > 0) {
-		buf[ret] = '\0';
-		printf("GOT: %s\n", buf);
+	int done = 0;
+	while (!done) {
+		pfds[0].events = pfds[1].events = POLLIN;
+		int ret;
+		if ((ret = poll(pfds, nfds, -1)) == -1)
+			err(-1, "poll");
+		if (ret == 0)
+			errx(-1, "what");
+		int i;
+		for (i = 0; i < nfds; i++) {
+			if ((pfds[i].revents & POLLIN) == 0)
+				continue;
+			ssize_t c = read(pfds[i].fd, buf, sizeof(buf) - 1);
+			if (c == -1) {
+				err(-1, "read");
+			} else if (c == 0) {
+				printf("fd %d closed\n", pfds[i].fd);
+				done = 1;
+				break;
+			}
+			buf[c] = '\0';
+			ssize_t w = write(i == 0 ? s : 1, buf, c);
+			if (w == -1)
+				err(-1, "write");
+			else if (w != c)
+				errx(-1, "short write");
+		}
 	}
-	if (ret < 0)
-		err(-1, "read");
-
+	printf("lnc finished\n");
 	return 0;
 }
