259a6c0049ed99328ce206f78864569a52dd2b29
Cody Cutler <ccutler@mat.lcs.mit.edu>
2017-04-03 01:30:10 -0400

    "unsafe" readn/writen
    improves mailbench throughput by 3%!

--- biscuit/syscall.go
+++ biscuit/syscall.go
@@ -4642,17 +4642,36 @@ func sys_info(proc *proc_t, n int) int {
 }
 
 func readn(a []uint8, n int, off int) int {
-	ret := 0
-	for i := 0; i < n; i++ {
-		ret |= int(a[off + i]) << (uint(i)*8)
+	p := unsafe.Pointer(&a[off])
+	var ret int
+	switch n {
+	case 8:
+		ret = *(*int)(p)
+	case 4:
+		ret = int(*(*uint32)(p))
+	case 2:
+		ret = int(*(*uint16)(p))
+	case 1:
+		ret = int(*(*uint8)(p))
+	default:
+		panic("no")
 	}
 	return ret
 }
 
 func writen(a []uint8, sz int, off int, val int) {
-	v := uint(val)
-	for i := 0; i < sz; i++ {
-		a[off + i] = uint8((v >> (uint(i)*8)) & 0xff)
+	p := unsafe.Pointer(&a[off])
+	switch sz {
+	case 8:
+		*(*int)(p) = val
+	case 4:
+		*(*uint32)(p) = uint32(val)
+	case 2:
+		*(*uint16)(p) = uint16(val)
+	case 1:
+		*(*uint8)(p) = uint8(val)
+	default:
+		panic("no")
 	}
 }
 
