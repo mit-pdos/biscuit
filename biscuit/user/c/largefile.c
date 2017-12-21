6f2f80f135aacbe803f5c92b4c2b2d5d39a2fb69
Frans Kaashoek <kaashoek@mit.edu>
2017-12-20 20:15:28 -0500

    Import largefile and smallfile from fscq/bench. Runs on biscuit.

---
+++ biscuit/user/c/largefile.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+/* Measure creating a large file and overwriting that file */
+
+#define WSIZE (4096)
+#define FILESIZE 50 * 1024 * 1024
+#define NAMESIZE 100
+
+static char name[NAMESIZE];
+static char buf[WSIZE];
+static char *prog;
+static char *dir;
+
+void printstats(int reset)
+{
+  int fd;
+  int r;
+
+  sprintf(name, "%s/stats", dir);
+  if((fd = open(name, O_RDONLY)) < 0) {
+    return;
+  }
+
+  memset(buf, 0, WSIZE);
+
+  if ((r = read(fd, buf, WSIZE)) < 0) {
+    perror("read");
+    exit(1);
+  }
+
+  if (!reset) fprintf(stdout, "=== FS Stats ===\n%s========\n", buf);
+
+  if ((r = close(fd)) < 0) {
+    perror("close");
+  }
+}
+
+void makefile()
+{
+  int i;
+  int fd;
+
+  int n = FILESIZE/WSIZE;
+  
+  sprintf(name, "%s/d/f", dir);
+  if((fd = open(name, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU)) < 0) {
+    printf("%s: create %s failed %s\n", prog, name, strerror(errno));
+    exit(1);
+  }
+
+  sprintf(buf, "%s/stats", dir);
+    
+  for (i = 0; i < n; i++) {
+    if (write(fd, buf, WSIZE) != WSIZE) {
+      printf("%s: write %s failed %s\n", prog, name, strerror(errno));
+      exit(1);
+    }
+  }
+  if (fsync(fd) < 0) {
+    printf("%s: fsync %s failed %s\n", prog, name, strerror(errno));
+    exit(1);
+  }
+
+  lseek(fd, SEEK_SET, 0);
+  write(fd, buf, WSIZE);
+  close(fd);
+  
+  fd = open(".", O_DIRECTORY | O_RDONLY);
+  if (fd < 0) {
+    perror("open dir");
+    exit(-1);
+  }
+  if (fsync(fd) < 0) {
+    perror("fsync");
+    exit(-1);
+  }
+}
+
+void writefile()
+{
+  int i;
+  int fd;
+
+  int n = FILESIZE/WSIZE;
+  
+  sprintf(name, "%s/d/f", dir);
+  if((fd = open(name, O_RDWR, S_IRWXU)) < 0) {
+    printf("%s: open %s failed %s\n", prog, name, strerror(errno));
+    exit(1);
+  }
+  
+  sprintf(buf, "%s/stats", dir);
+  
+  for (i = 0; i < n; i++) {
+    if (write(fd, buf, WSIZE) != WSIZE) {
+      printf("%s: write %s failed %s\n", prog, name, strerror(errno));
+      exit(1);
+    }
+    if (((i + 1) * WSIZE) % (10 * 1024 * 1024) == 0) {
+      if (fsync(fd) < 0) {
+	  printf("%s: fsync %s failed %s\n", prog, name, strerror(errno));
+	  exit(1);
+	}
+    }
+  }
+  if ((i * WSIZE) % (10 * 1024 * 1024) != 0 && fsync(fd) < 0) {
+    printf("%s: fsync %s failed %s\n", prog, name, strerror(errno));
+    exit(1);
+  }
+  close(fd);
+}
+
+int main(int argc, char *argv[])
+{
+  long time;
+  struct timeval before;
+  struct timeval after;
+  float tput;
+
+  if (argc != 2) {
+    printf("Usage: %s basedir\n", argv[0]);
+    exit(-1);
+  }
+  
+  prog = argv[0];
+  dir = argv[1];
+  sprintf(name, "%s/d", dir);
+  if (mkdir(name,  S_IRWXU) < 0) {
+    printf("%s: create %s failed %s\n", prog, name, strerror(errno));
+    exit(1);
+  }
+
+  printstats(1);
+    
+  gettimeofday ( &before, NULL );  
+  makefile();
+  gettimeofday ( &after, NULL );
+
+  time = (after.tv_sec - before.tv_sec) * 1000000 +
+	(after.tv_usec - before.tv_usec);
+  tput = ((float) (FILESIZE/1024) /  (time / 1000000.0));
+  printf("makefile %d MB %ld usec throughput %5.1f KB/s\n", FILESIZE/(1024*1024), time, tput);
+
+  printstats(0);
+    
+  gettimeofday ( &before, NULL );  
+  writefile();
+  gettimeofday ( &after, NULL );
+  
+  time = (after.tv_sec - before.tv_sec) * 1000000 +
+	(after.tv_usec - before.tv_usec);
+  tput = ((float) (FILESIZE/1024) /  (time / 1000000.0));
+  printf("writefile %d MB %ld usec throughput %5.1f KB/s\n", FILESIZE/(1024*1024), time, tput);
+
+  printstats(0);
+  return 0;
+}
---
+++ biscuit/user/c/smallfile.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#define N 1000
+#define FILESIZE 100
+#define NAMESIZE 100
+
+static char name[NAMESIZE];
+static char buf[FILESIZE]; 
+static char *dir;
+
+void printstats(int reset)
+{
+  int fd;
+  int r;
+
+  sprintf(name, "%s/stats", dir);
+  if((fd = open(name, O_RDONLY)) < 0) {
+    return;
+  }
+
+  memset(buf, 0, FILESIZE);
+
+  if ((r = read(fd, buf, FILESIZE)) < 0) {
+    perror("read");
+    exit(1);
+  }
+
+  if (!reset)
+    fprintf(stdout, "=== FS Stats ===\n%s========\n", buf);
+
+  if ((r = close(fd)) < 0) {
+    perror("close");
+  }
+}
+
+static uint64_t
+usec_now()
+{
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+
+  uint64_t res = tv.tv_sec;
+  res *= 1000000;
+  res += tv.tv_usec;
+  return res;
+}
+
+int main(int argc, char *argv[])
+{
+  int i;
+  int fd;
+  uint64_t start, end;
+
+  if (argc != 2) {
+    printf("Usage: %s basedir\n", argv[0]);
+    exit(-1);
+  }
+  
+  dir = argv[1];
+  sprintf(buf, "%s/d", argv[1]);
+  if (mkdir(buf,  S_IRWXU) < 0) {
+    printf("%s: create %s failed %s\n", argv[0], buf, strerror(errno));
+    exit(1);
+  }
+  printstats(1);
+  start = usec_now();
+  for (i = 0; ; i++) {
+    sprintf(name, "%s/d/f%d", argv[1], i);
+    if((fd = open(name, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU)) < 0) {
+      printf("%s: create %s failed %s\n", argv[0], name, strerror(errno));
+      exit(1);
+    }
+    if (write(fd, buf, FILESIZE) != FILESIZE) {
+      printf("%s: write %s failed %s\n", argv[0], name, strerror(errno));
+      exit(1);
+    }
+    if (fsync(fd) < 0) {
+      printf("%s: fsync %s failed %s\n", argv[0], name, strerror(errno));
+      exit(1);
+    }
+    close(fd);
+
+    end = usec_now();
+    if (end - start > 1000000)
+      break;
+  }
+
+  printf("%d files per %ld usec\n", i, end-start);
+
+  printstats(0);
+  return 0;
+}
