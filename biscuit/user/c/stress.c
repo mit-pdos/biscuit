760539ce62c33b270c7b093cf3ec67e560dfc118
Cody Cutler <ccutler@csail.mit.edu>
2018-03-16 15:47:28 -0400

    new naughty program
    grow kernel heap using VMAs

--- biscuit/user/c/stress.c
+++ biscuit/user/c/stress.c
@@ -1,142 +1,54 @@
 #include <stdio.h>
 
-void dirents(void)
-{
-	char *path = "/tmp/dcbloat";
-	mkdir(path, 0755);
-	if (chdir(path) == -1)
-		err(-1, "chdir");
-	for (;;) {
-		// fill up vnode and dirent cache
-		for (;;) {
-			int fd = mkstemp("XXXXXX");
-			if (fd == -1)
-				break;
-			if (close(fd) == -1)
-				err(-1, "close");
-		}
-		DIR *d;
-		while (!(d = opendir("."))) {
-			printf("-O-");
-			usleep(10000);
-		}
-		struct dirent di, *de;
-		int i;
-		for (i = 0; i < 1000; i++) {
-			while (readdir_r(d, &di, &de) != 0) {
-				printf("-R-");
-				usleep(10000);
-			}
-			if (de == NULL) {
-				printf("-R-");
-				usleep(10000);
-			}
-			if (strcmp(de->d_name, "..") == 0 ||
-			    strcmp(de->d_name, ".") == 0)
-				continue;
-			if (unlink(de->d_name) == -1)
-				errx(-1, "unlink");
-		}
-		if (closedir(d) == -1)
-			err(-1, "closedir");
-		printf("REMMED %d\n", i);
-	}
-}
-
-void gcstat(void)
-{
-	int stgcs;
-	if ((stgcs = sys_info(SINFO_GCCOUNT)) == -1)
-		err(-1, "sysinfo");
-	for (;;) {
-		sleep(5);
-		//int gcs;
-		//if ((gcs = sys_info(SINFO_GCCOUNT)) == -1)
-		//	err(-1, "sysinfo");
-		//printf("GCs: %d\n", gcs - stgcs);
-		//stgcs = gcs;
-		//sys_info(SINFO_GCHEAPSZ);
-		sys_info(10);
-	}
-}
-
 int main(int argc, char **argv)
 {
 	srand(time(NULL));
-	pid_t master = getpid();
-	switch (fork()) {
-	case 0:
-		dirents();
-	case -1:
-		err(-1, "fork");
-	}
+	//pid_t master = getpid();
 	//switch (fork()) {
 	//case 0:
-	//	gcstat();
+	//	dirents();
 	//case -1:
 	//	err(-1, "fork");
 	//}
+
+	int done = 0;
+	for (int i = 0; i < 10 && !done; i++) {
+		switch (fork()) {
+		case -1:
+			err(-1, "fork");
+			break;
+		case 0:
+			done = 1;
+			break;
+		}
+	}
+
 
 	int afd, bfd;
 	if ((afd = open("/", O_RDONLY | O_DIRECTORY)) == -1)
 		err(-1, "open");
 	if ((bfd = open("/", O_RDONLY | O_DIRECTORY)) == -1)
 		err(-1, "open");
-	int i;
-yahoo:
-	for (i = 0; i < 100; i++) {
-		pid_t c = fork();
-		if (c == 0)
-			i = -1;
-		else if (c == -1)
-			break;
-	}
-	//int upto = i;
 
-	// max size fd tables
-	int do1, do2, do3;
-	do1 = do2 = do3 = 1;
-	pid_t mypid = getpid();
-	while (mypid != master && (do1 || do2 || do3)) {
-		if (do1 && open("/", O_RDONLY | O_DIRECTORY) == -1)
-			do1 = 0;
-		// alternate to make sure vma objects aren't coalesced.
-		if (do2) {
-			void *r = mmap(NULL, 4096, PROT_READ, MAP_PRIVATE, afd, 0);
-			if (r == MAP_FAILED)
-				do2 = 0;
-			r = mmap(NULL, 4096, PROT_READ, MAP_PRIVATE, bfd, 0);
-			if (r == MAP_FAILED)
-				do2 = 0;
-		}
-		if (do3) {
-			int s;
-			if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-				do3 = 0;
-			struct sockaddr_in _sa;
-			_sa.sin_family = AF_INET;
-			_sa.sin_port = htons(10);
-			// connect to hacked IP which fails after allocating
-			// TCB data structures
-			_sa.sin_addr.s_addr = htonl(0xaaaaaaaa);
-			struct sockaddr *sa = (struct sockaddr *)&_sa;
-			if (connect(s, sa, sizeof(_sa)) == -1 &&
-			    errno == -ENOMEM)
-				do3 = 0;
-		}
+	char buf[512];
+	sprintf(buf, "%ld", getpid());
+	for (;;) {
+		if (open(buf, O_CREAT | O_RDWR, 0600) == -1)
+			break;
+		if (unlink(buf) == -1)
+			err(-1, "unlink");
 	}
-	//for (i = 0; i < upto; i++)
-	//	if (waitpid(pids[i], NULL, 0) == -1)
-	//		err(-1, "wait");
-	if (getpid() == master) {
-		pid_t w;
-		while ((w = wait4(WAIT_ANY, NULL, WNOHANG, NULL)) > 0)
-			;
-		if (w == -1 && errno != ECHILD)
-			err(-1, "master");
-		sleep(rand() % 10);
-		printf("again %d\n", i);
-		goto yahoo;
+
+	for (;;) {
+		void *p = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+		    MAP_PRIVATE, afd, 0);
+		if (p == MAP_FAILED)
+			err(-1, "mmap");
+		p = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+		    MAP_PRIVATE, bfd, 0);
+		if (p == MAP_FAILED)
+			err(-1, "mmap");
 	}
-	exit(0);
+
+	return 0;
 }
