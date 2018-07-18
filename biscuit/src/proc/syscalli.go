87f5bf48bdffafc9c33b0c02b8559e6050f1ec9d
Frans Kaashoek <kaashoek@mit.edu>
2018-07-18 19:56:46 -0400

    x

--- biscuit/src/proc/syscalli.go
+++ biscuit/src/proc/syscalli.go
@@ -3,6 +3,7 @@ package proc
 import "defs"
 import "fdops"
 
+// XXX add all syscalls so that we easily can do syscall interposition.  currently no use.
 type Syscall_i interface {
 	Syscall(p *Proc_t, tid defs.Tid_t, tf *[defs.TFSIZE]uintptr) int
 	Sys_close(proc *Proc_t, fdn int) int
