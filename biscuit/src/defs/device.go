38d7c136c9e8434a477fd8c27444cb4f97f89bfa
Frans Kaashoek <kaashoek@mit.edu>
2018-07-18 21:09:10 -0400

    oops, forgot to add a file

---
+++ biscuit/src/defs/device.go
@@ -0,0 +1,14 @@
+package defs
+
+const (
+	D_CONSOLE int = 1
+	// UNIX domain sockets
+	D_SUD     = 2
+	D_SUS     = 3
+	D_DEVNULL = 4
+	D_RAWDISK = 5
+	D_STAT    = 6
+	D_PROF    = 7
+	D_FIRST   = D_CONSOLE
+	D_LAST    = D_SUS
+)
