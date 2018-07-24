5b4c503303f986832dcdba5ca90e9eb36092cff3
Frans Kaashoek <kaashoek@mit.edu>
2018-07-24 17:05:22 -0400

    Checkpoint: made one connection between client and server successfully

--- biscuit/src/unet/unet_test.go
+++ biscuit/src/unet/unet_test.go
@@ -9,6 +9,8 @@ import "fdops"
 import . "inet"
 import "log"
 import "mem"
+import "res"
+import "vm"
 
 type pktmem_t struct {
 }
@@ -43,7 +45,7 @@ func TestConnect(t *testing.T) {
 	var mem = &pktmem_t{}
 
 	bnet.Net_init(mem)
-
+	res.Kernel = false
 	var opts defs.Fdopt_t
 
 	rcv := &bnet.Tcpfops_t{}
@@ -54,57 +56,72 @@ func TestConnect(t *testing.T) {
 	fam := make([]uint8, 2)
 	fam[1] = defs.AF_INET
 
-	lip := Ip4_t(0x7f000001)
-	lipsa := make([]uint8, 4)
-	Ip2sl(lipsa, lip)
+	inip := Ip4_t(defs.INADDR_ANY)
+	inipsa := make([]uint8, 4)
+	Ip2sl(inipsa, inip)
 
 	sportsa := make([]uint8, 2)
 	sport := 1090
 	sportsa[0] = uint8(sport >> 8)
 	sportsa[1] = uint8(sport >> 0)
 
-	fmt.Printf("TestConnet %s\n", Ip2str(lip))
+	fmt.Printf("TestConnet\n")
 
 	sa := append(fam, sportsa...)
-	sa = append(sa, lipsa...)
+	sa = append(sa, inipsa...)
 
 	err := rcv.Bind(sa)
 	if err != 0 {
 		t.Fatalf("Bind %d\n", err)
 	}
 
+	conn, err := rcv.Listen(10)
+	if err != 0 {
+		t.Fatalf("Listen %d\n", err)
+	}
+
 	ch := make(chan bool)
-	go func() {
+	go func(conn fdops.Fdops_i, t *testing.T) {
+		log.Printf("wait for accept\n")
 		for true {
-			log.Printf("listen\n")
-			ch <- true
-			conn, err := rcv.Listen(10)
+			hdata := make([]uint8, 8)
+			ub := &vm.Fakeubuf_t{}
+			ub.Fake_init(hdata)
+
+			clnt, _, err := conn.Accept(ub)
 			if err != 0 {
-				t.Fatalf("Listen %d\n", err)
+				t.Fatalf("accept")
 			}
-			go func(conn fdops.Fdops_i) {
-				log.Printf("close connection\n")
-				conn.Close()
-			}(conn)
+			ipsa := hdata[4:]
+			ip := Sl2ip(ipsa)
+			p := hdata[2]<<8 | hdata[3]
+			log.Printf("accepted clnt (%s,%d)\n", Ip2str(ip), p)
+			ch <- true
+			clnt.Close()
+
 		}
-	}()
+	}(conn, t)
 
-	<-ch
+	log.Printf("call connect %d\n", 1090)
 
-	log.Printf("call connect\n")
-
+	dip := Ip4_t(0x7f000001)
+	dipsa := make([]uint8, 4)
+	Ip2sl(dipsa, dip)
 	dportsa := make([]uint8, 2)
 	dport := 1090
 	dportsa[0] = uint8(dport >> 8)
 	dportsa[1] = uint8(dport >> 0)
 
 	sa = append(fam, dportsa...)
-	sa = append(sa, lipsa...)
+	sa = append(sa, dipsa...)
 
 	err = snd.Connect(sa)
 	if err != 0 {
 		t.Fatalf("connect %d", err)
 	}
+
+	<-ch
+
 	err = snd.Close()
 	if err != 0 {
 		t.Fatalf("close %d", err)
